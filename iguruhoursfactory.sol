1c33fc1eaa2b43cccea2329c647c9d5285855281
Ilya Svirin <i.svirin@nordavind.ru>
2022-09-01 15:35:20 +0300

    Add IGuruHoursFactory interface for GutuHoursToken and GuruHoursFactory interaction

---
+++ iguruhoursfactory.sol
@@ -0,0 +1,33 @@
+/*
+This file is part of the Guru Collective DAO.
+
+The IGuruHoursFactory Contract is free software: you can redistribute it and/or
+modify it under the terms of the GNU lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The IGuruHoursFactory Contract is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU lesser General Public License for more details.
+
+You should have received a copy of the GNU lesser General Public License
+along with the IGuruHoursFactory Contract. If not, see <http://www.gnu.org/licenses/>.
+
+@author Ilya Svirin <is.svirin@gmail.com>
+*/
+// SPDX-License-Identifier: GNU lesser General Public License
+
+pragma solidity ^0.8.0;
+
+
+interface IGuruHoursFactory
+{
+    function fee() view external returns(uint256);
+    function dao() view external returns(address);
+    function onTransfer(address sender, address recipient, uint256 amount) external;
+    function onChangeName(string memory name) external;
+    function onChangeBio(string memory bioCID) external;
+    function onChangeAvatar(string memory avatarCID) external;
+    function onOwnershipTransfer(address newOwner) external;
+}
